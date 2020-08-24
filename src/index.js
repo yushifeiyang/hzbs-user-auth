bc00e692cc93b778ccc0fa428350a7ec52e1821f
云阳间 <1114744193@qq.com>
2020-08-24 10:21:01 +0800

    增加jquery支持

--- src/index.js
+++ src/index.js
@@ -2,40 +2,66 @@
 内容:权限文件
 作用:为了统一代码,将这个文件发包到npm,供其他项目使用
 */
- const appAuth = (urlParams, axios, callback, errorPage, domain, next) => {
+// 在app里验证权限
+ const appAuth = (urlParams, axios, callback, errorPage, domain, next, isJQuery) => {
 	// 获取事项权限
-	getItemInfo(axios, urlParams, domain, vertifyUserAuth, callback, errorPage, next);
+	getItemInfo(axios, urlParams, domain, vertifyUserAuth, callback, errorPage, next, isJQuery);
 }
 
+// 非app验证权限
+const h5Auth = () => {}
+
 // 获取事项权限
-function getItemInfo(axios, urlParams, domain, callback, topCallback, errorPage, next) {
+function getItemInfo(axios, urlParams, domain, callback, topCallback, errorPage, next, isJQuery) {
 	const url = `${domain}/unite/${urlParams.itemNo}?srcCode=${urlParams.channelCode}&applicationId=${urlParams.applicationId}&kg=1`;
 	// 异常统一在axios里处理,这里不做处理
-	axios.post(url, urlParams).then(itemAuth => {
-		console.log('itemAuth res', itemAuth);
-		if (itemAuth.errorCode.toString() === '0') {
-			if (itemAuth.data.flag === '0') { // 状态有效
-				if (itemAuth.data.loginFlag === '1') { // {1:需要登入，0：不需要}
-					// verifyApi(itemNo, channelCode, to.path, next, applicationId, to)
-					callback(axios, urlParams, domain, topCallback, next);
-				} else { // 游客登入
+	if (isJQuery) {
+		axios.ajax({
+			url: url,
+			type: 'post',
+			xhrFields: {
+				withCredentials: true
+			},
+			data: urlParams,
+			crossDomain: true,
+			success: function (itemAuth) {
+				if (itemAuth.errorCode.toString() === '0') {
+					if (itemAuth.data.flag === '0') { // 状态有效
+						if (itemAuth.data.loginFlag === '1') { // {1:需要登入，0：不需要}
+							callback(axios, urlParams, domain, topCallback, next, isJQuery);
+						}
+					}
+				} else {
+					console.log('事项权限，暂时没处理失败');
+				}
+			}
+		});
+	} else {
+		axios.post(url, urlParams).then(itemAuth => {
+			console.log('itemAuth res', itemAuth);
+			if (itemAuth.errorCode.toString() === '0') {
+				if (itemAuth.data.flag === '0') { // 状态有效
+					if (itemAuth.data.loginFlag === '1') { // {1:需要登入，0：不需要}
+						callback(axios, urlParams, domain, topCallback, next);
+					} else { // 游客登入
+						next()
+					}
+				} else {
 					next()
 				}
 			} else {
-				next()
+				next({
+					path: errorPage,
+					query: {
+						errorMsg: itemAuth.value
+					}
+				})
 			}
-		} else {
-			next({
-				path: errorPage,
-				query: {
-					errorMsg: itemAuth.value
-				}
-			})
-		}
-	});
+		});
+	}
 }
 // 获取用户权限
-function vertifyUserAuth(axios, urlParams, domain, callback, next) {
+function vertifyUserAuth(axios, urlParams, domain, callback, next, isJQuery) {
 	if (process.env.NODE_ENV === 'development') {
 		const url = `${domain}/unite/${urlParams.itemNo}/11111111?srcCode=${urlParams.channelCode}&applicationId=${urlParams.applicationId}&kg=1`;
 		axios.post(url, urlParams).then(userAuth => {
@@ -46,48 +72,25 @@ function vertifyUserAuth(axios, urlParams, domain, callback, next) {
 			onSuccess: (res) => {
 				const url = `${domain}/unite/${urlParams.itemNo}/${res.param.authCode}?srcCode=${urlParams.channelCode}&applicationId=${urlParams.applicationId}&kg=1`;
 				// 异常统一在axios里处理,这里不做处理
-				axios.post(url, urlParams).then(userAuth => {
-					console.log('userAuth res', userAuth);
-					if (userAuth.errorCode.toString() === '0') {
-						callback(next);
-					} else if (res.errorCode.toString() === '10001') {
-						// // 需先登录
-						window.yl.call('getAuthcode', {}, {
-							onSuccess: function (res) {
-								window.yl.call('closeWebview')
-							},
-							onFail: function (res) {
-								window.yl.call('closeWebview')
-							}
-						})
-					} else if (res.errorCode.toString() === '10002') {
-						// 需先实名登记
-						window.yl.call('requireAuth', {}, {
-							onSuccess: function (res) {
-								window.yl.call('closeWebview')
-							},
-							onFail: function (res) {
-								window.yl.call('closeWebview')
-							}
-						})
-					} else if (res.errorCode.toString() === '10003') {
-						window.yl.call('faceDetection', {}, {
-							onSuccess: function (res) {
-								window.yl.call('closeWebview')
-							},
-							onFail: function (res) {
-								window.yl.call('closeWebview')
-							}
-						})
-					} else {
-						next({
-							path: '/submitFail',
-							query: {
-								errorMsg: res.value
-							}
-						})
-					}
-				})
+				if (isJQuery) {
+					axios.ajax({
+						url: url,
+						type: 'post',
+						xhrFields: {
+							withCredentials: true
+						},
+						data: urlParams,
+						crossDomain: true,
+						success: function (userAuth) {
+							userAuthHanddle(userAuth, null);
+						}
+					});
+				} else {
+					axios.post(url, urlParams).then(userAuth => {
+						console.log('userAuth res', userAuth);
+						userAuthHanddle(userAuth, next);
+					});
+				}
 			},
 			onFail: (e) => {
 				console.log('获取authCode失败');
@@ -97,4 +100,51 @@ function vertifyUserAuth(axios, urlParams, domain, callback, next) {
 	}
 }
 
+function userAuthHanddle(userAuth, next) {
+	if (userAuth.errorCode.toString() === '0') {
+		callback(next);
+	} else if (userAuth.errorCode.toString() === '10001') {
+		// // 需先登录
+		window.yl.call('getAuthcode', {}, {
+			onSuccess: function (e) {
+				window.yl.call('closeWebview')
+			},
+			onFail: function (e) {
+				window.yl.call('closeWebview')
+			}
+		})
+	} else if (userAuth.errorCode.toString() === '10002') {
+		// 需先实名登记
+		window.yl.call('requireAuth', {}, {
+			onSuccess: function (e) {
+				window.yl.call('closeWebview')
+			},
+			onFail: function (e) {
+				window.yl.call('closeWebview')
+			}
+		})
+	} else if (userAuth.errorCode.toString() === '10003') {
+		window.yl.call('faceDetection', {}, {
+			onSuccess: function (e) {
+				window.yl.call('closeWebview')
+			},
+			onFail: function (e) {
+				window.yl.call('closeWebview')
+			}
+		})
+	} else {
+		if (next) {
+			next({
+				path: '/submitFail',
+				query: {
+					errorMsg: res.value
+				}
+			})
+		} else {
+			console.log('用户权限，暂时没处理失败');
+		}
+	}
+}
+
 exports.appAuth = appAuth;
+exports.h5Auth = h5Auth;
